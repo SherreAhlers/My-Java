89fd8fdee920a94fb8949bb0f5805a31cc4bed7f
SherreAhlers <sherre.ahlers@gmail.com>
2020-12-09 13:10:56 -0600

    Java - hello world

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        // public is an access modifier we discussed when defining the class.
+        // static is a Java keyword that needs an understanding of other concepts, for now, know that we need to have static
+        // for Java to find our method to run the code we are going to add.
+        // void is yet another Java keyword used to indicate that the method will not return anything.
+        // the left and right parenthesis in a method declaration are mandatory and can optionally include one or more parameters
+        // which is a way to pass information to a method.
+        System.out.println("Hello World");
+        // must have semi colon to end statement or will get an error.
+
+    }
+}
+// exit code 0 means eveything was executed properly
+
+// code block is used to define a block of code. Its mandatory to have one in a method declaration and its here where we will be adding statements to perform certain tasks.
+
+// statement - this is a complete command to be executed and can include one or more expressions.
