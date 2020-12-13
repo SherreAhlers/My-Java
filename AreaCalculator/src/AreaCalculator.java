72e45ba5e4a7b4467b94a0fd3f598c79d5040a82
SherreAhlers <sherre.ahlers@gmail.com>
2020-12-13 14:45:07 -0600

    Area Calculator Challenge

---
+++ AreaCalculator/src/AreaCalculator.java
@@ -0,0 +1,35 @@
+public class AreaCalculator {
+    // CHALLENGE
+    // Write a method named area with one double parameter named radius
+    // The method needs to return a double value that represents the area of a circle.
+    // If the parameter radius is negative then return -1.0 to represent the invalid value
+
+    // Write another overloaded method with 2 parameters x and y (both doubles)
+    // where x and y represent the sides of a rectangle.
+
+    // The method needs to return an area of a rectangle
+
+    // If either or both parameters is/are a negative return -1.0 to indicate an invalid value.
+
+    // TIPS:
+    // The formula for calculating the area of a rectangle is x*y
+    // The formula for calculating a circle area is radius*radius*PI
+    // For PI use a constant from Math class e.g. Math.PI
+
+    // MY SOLUTION = CORRECT!
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1.0;
+        }
+        double circleArea = Math.PI * (radius * radius);
+        return circleArea;
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1.0;
+        }
+        double rectangleArea = x * y;
+        return rectangleArea;
+    }
+}
