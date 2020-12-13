baf00b50a084a17dc34d72155153808ff7fa9a1a
SherreAhlers <sherre.ahlers@gmail.com>
2020-12-13 12:33:17 -0600

    Equal Sum Checker Challenge

---
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+    //  MY SOLUTION - CORRECT!
+    public static boolean hasEqualSum(int one, int two, int three) {
+        if(one + two == three) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
