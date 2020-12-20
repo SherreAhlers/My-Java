138fb4317ebc4991da2ae6c3f66d73a3727f33c1
SherreAhlers <sherre.ahlers@gmail.com>
2020-12-20 16:28:56 -0600

    Cylinder Challenge

---
+++ Cylinder/src/Cylinder.java
@@ -0,0 +1,33 @@
+
+// 2. Write a class with the name Cylinder that extends Circle class.
+// The class needs one field (instance variable) with the name height
+// of type double.
+// The class needs to have one constructor with two parameters radius
+// and height both of type double. It needs to call parent constructor
+// and initialize a height field.
+// In case the height parameter is less than 0 it needs to set the height
+// field value to 0.
+// Write the following methods:
+// Method named getHeight without parameters, it needs to return the
+// calculated volume. To calculate volume multiply the area with height.
+
+// MY VERSION _ CORRECT!!
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+    public double getVolume() {
+        return getArea() * this.height;
+    }
+}
