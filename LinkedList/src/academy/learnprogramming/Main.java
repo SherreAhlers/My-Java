f0a85ddb36936ae6e66dd45b0ab04c141d4671da
SherreAhlers <sherre.ahlers@gmail.com>
2020-12-26 12:20:09 -0600

    Linked Lists

---
+++ LinkedList/src/academy/learnprogramming/Main.java
@@ -0,0 +1,64 @@
+package academy.learnprogramming;
+
+import java.util.ArrayList;
+
+public class Main {
+    // Linked list - help computer process quickly
+    // if have to remove an item from array all other elements must be moved
+    // up to fill the gap.
+    // Linked list - alternative for array
+    // it works by storing a link to the last element and data
+    // first element points to the next element which then points to the next
+    // element.
+    // this allows us to iterate through all elements
+    // if you add an element it will figure out what it connects to and delete
+    // whatever has been bypassed.
+
+
+    public static void main(String[] args) {
+//         // int takes up 4 bytes in memory
+//        Customer customer = new Customer("Tim", 54.96);
+//        Customer anotherCustomer;
+//        anotherCustomer = customer;
+//        anotherCustomer.setBalance(12.18);
+//        System.out.println("Balance for customer " +
+//                customer.getName() + " is " + customer.getBalance());
+//        // this will output: Balance for customer Tim is 12.18
+//        // what happens when we do assignment anotherCustomer = customer;
+//        // java is assigning the anotherCustomer class to point two the first class
+//        // didn't create a second class
+//        // only saved memory address.
+//        // setBalance() will save the balance to the first spot in memory
+//        // overriding it.
+//        ArrayList<Integer> intList = new ArrayList<Integer>();
+//        // code creates a basic array list
+//
+//        intList.add(1);
+//        // adding values into array
+//        intList.add(3);
+//        // adding values into array
+//        intList.add(4);
+//        // adding values into array
+//
+//        for(int i = 0; i < intList.size(); i++) {
+//            System.out.println(i + ": " + intList.get(i));
+//            // this will show output of each element in the array.
+//            // this will output:
+//            // 0: 1
+//            // 1: 3
+//            // 2: 4
+//        }
+//        intList.add(1, 2);
+//        // here we are inserting the number 2 into index position 1 and storing it.
+//
+//        for(int i = 0; i < intList.size(); i++) {
+//            System.out.println(i + ": " + intList.get(i));
+//            // this will output:
+//            // 0: 1
+//            // 1: 2
+//            // 2: 3
+//            // 3: 4
+//        }
+    }
+        // A linked list points to the next one and the next one and the next one
+}
