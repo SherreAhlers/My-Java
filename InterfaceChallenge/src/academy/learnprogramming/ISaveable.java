fd449fb5ef0327c4402c3bfef86945928abf6494
SherreAhlers <sherre.ahlers@gmail.com>
2020-12-26 16:09:12 -0600

    Interface Challenge

---
+++ InterfaceChallenge/src/academy/learnprogramming/ISaveable.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> savedValues);
+
+    }
+
